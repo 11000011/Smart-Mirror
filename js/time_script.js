75d32c098b59aa58b289627ae33065ff846196e6
Yash Srivastav <yashsriv01@gmail.com>
2016-05-25 14:05:10 +0530

    Added welcome screen

---
+++ js/time_script.js
@@ -0,0 +1,20 @@
+window.requestAnimationFrame = window.requestAnimationFrame
+                            || window.mozRequestAnimationFrame
+                            || window.webkitRequestAnimationFrame
+                            || window.msRequestAnimationFrame
+                            || function(f) { return setTimeout(f, 1000 / 60)};
+
+function updateclock() {
+  var curdate = new Date()
+  var hour = curdate.getHours()
+  var minute = curdate.getMinutes()
+  var second = curdate.getSeconds()
+  $(".time").html(hour + " : " + minute + " : " + second)
+  requestAnimationFrame(updateclock)
+}
+
+$(document).ready(function() {
+  $("#intro").fadeOut(3000, function() {
+    requestAnimationFrame(updateclock);
+  });
+});
