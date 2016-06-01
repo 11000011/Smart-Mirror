d87c53d8ea1f4c93302d218a5a4ed9662451a001
jmoriarty221B <abhi.jmoriarty@gmail.com>
2016-06-01 11:05:06 +0530

    properly indented

--- js/weather_script.js
+++ js/weather_script.js
@@ -1,26 +1,26 @@
 var icons = new Skycons({"color": "#EEEEEE"});
 function getweather() {
-    $.ajax({
-      url: "http://ip-api.com/json/?callback=?",
-      type: "GET",
-      dataType: "json",
-    })
-    .done(function(ip){
-      console.log(ip.lat)
-        console.log(ip.lon)
-        $.ajax({
-          url:"https://api.forecast.io/forecast/70a79cc9ef81d5d038f26f0163b83d22/" + ip.lat + "," + ip.lon + "?callback=?",
-          type: "GET",
-          dataType: "json",
-        })
-      .done(function(weather){
-        console.log(weather)
-        icons.set("weather", weather.currently.icon);
-        var t = weather.currently.temperature;
-        t = (t - 32) * 5 / 9.0;
-        t = Math.round( t * 1 ) / 1;
-        $("#temp").html(t + "°" );
-        $(".icondes").html(weather.currently.icon);
+  $.ajax({
+    url: "http://ip-api.com/json/?callback=?",
+    type: "GET",
+    dataType: "json",
+  })
+  .done(function(ip){
+    console.log(ip.lat)
+      console.log(ip.lon)
+      $.ajax({
+        url:"https://api.forecast.io/forecast/70a79cc9ef81d5d038f26f0163b83d22/" + ip.lat + "," + ip.lon + "?callback=?",
+        type: "GET",
+        dataType: "json",
       })
+    .done(function(weather){
+      console.log(weather)
+        icons.set("weather", weather.currently.icon);
+      var t = weather.currently.temperature;
+      t = (t - 32) * 5 / 9.0;
+      t = Math.round( t * 1 ) / 1;
+      $("#temp").html(t + "°" );
+      $(".icondes").html(weather.currently.icon);
     })
+  })
 }
