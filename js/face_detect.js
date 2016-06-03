6b1c97f7e914d16d86e34b5227f644c7c132d939
Shubham Jain <shbhmjn13@gmail.com>
2016-06-03 18:25:13 +0530

    Cleaned up face_detect.js (read comments)

--- js/face_detect.js
+++ js/face_detect.js
@@ -2,73 +2,57 @@ window.onload = function() {
 
   var smoother = new Smoother([0.9999999, 0.9999999, 0.999, 0.999], [0, 0, 0, 0]),
   video = document.getElementById('video'),
-  glasses = document.getElementById('glasses'),
   detector;
+  var delay = 3000;
 
-  try {
-    compatibility.getUserMedia({video: true}, function(stream) {
-      try {
-        video.src = compatibility.URL.createObjectURL(stream);
-      } catch (error) {
-        video.src = stream;
-      }
-      compatibility.requestAnimationFrame(play);
-    }, function (error) {
-      alert('WebRTC not available');
-    });
-  } catch (error) {
-    alert(error);
+  //function for checking if there is error in streaming of video or jot
+  function vider_check(){
+    try {
+      compatibility.getUserMedia({video: true}, function(stream) {
+        try {
+          video.src = compatibility.URL.createObjectURL(stream);
+        } catch (error) {
+          video.src = stream;
+        }
+        compatibility.requestAnimationFrame(play);
+      }, function (error) {
+        alert('WebRTC not available');
+      });
+    } 
+    catch (error) {
+      alert(error);
+    }
   }
 
+  //function playing the video and check for face
   function play() {
     compatibility.requestAnimationFrame(play);
     if (video.paused) video.play();
 
+    //function for checking the presence of face (returns YES or NO)
+    function check(){
+      if (video.readyState === video.HAVE_ENOUGH_DATA && video.videoWidth > 0) {
 
-    if (video.readyState === video.HAVE_ENOUGH_DATA && video.videoWidth > 0) {
-
-      //Prepare the detector once the video dimensions are known:
+        //Prepare the detector once the video dimensions are known:
         if (!detector) {
           var width = ~~(60 * video.videoWidth / video.videoHeight);
           var height  =60;
           detector = new objectdetect.detector(width, height, 1.1, objectdetect.frontalface_alt);
         }
 
-      // Perform the actual detection:
-      var coords = detector.detect(video, 1);
-      if (coords[0]) {
+        // Perform the actual detection:
+        var coords = detector.detect(video, 1);
+        if (coords[0]) {
 
-        console.log("YES");
-        //If there is a face you will be in this loop, otherwise it keeps checking
-        //Can call the Microsoft API from here
+          console.log("YES");
+          //If there is a face you will be in this loop, otherwise it keeps checking
+          //Can call the Microsoft API from here
 
-       /* var coord = coords[0];
-        coord = smoother.smooth(coord);
-        // Rescale coordinates from detector to video coordinate space:
-        coord[0] *= video.videoWidth / detector.canvas.width;
-        coord[1] *= video.videoHeight / detector.canvas.height;
-        coord[2] *= video.videoWidth / detector.canvas.width;
-        coord[3] *= video.videoHeight / detector.canvas.height;
-
-        var canvas = document.getElementById("canvas");
-        canvas.width = video.videoWidth;
-        canvas.height = video.videoHeight;
-        var ctx = canvas.getContext("2d");
-        ctx.drawImage(video , 0 , 0 , video.videoWidth , video.videoHeight , coord[0] , coord[1] , coord[2] , coord[3]);
-        var dataURL = canvas.toDataURL();
-        console.log(dataURL);*/
-
-      } else {
-        var opacity = glasses.style.opacity - 0.2;
-        glasses.style.opacity = opacity > 0 ? opacity : 0;
+        } 
+        else {
+          console.log("No");
+        }
       }
     }
   }
-
-  [].slice.call(document.getElementById('list').children).forEach(function(e) {
-    e.addEventListener('click', function() {
-      glasses.src = e.src;
-    }, false);
-  });
 };
-
