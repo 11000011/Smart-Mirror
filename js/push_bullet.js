1ffd627a13a0632230fb88b11f84f5eb5cc181bd
Subhdeep Saha <subhdeepsaha@gmail.com>
2016-06-25 23:42:00 +0530

    Coloured notification

--- js/push_bullet.js
+++ js/push_bullet.js
@@ -12,11 +12,24 @@ function get_noti() {
   }
   websocket.onmessage = function(e) {
     var data = JSON.parse(e.data);
+    console.log(data);
     if (data.type == "push") {
       if (data.push.type == "mirror") {
         var temp = "#" + data.push.notification_id;
         $(temp).remove();
-        $("#push_bullet ul").append('<li id ="' + data.push.notification_id + '">' + data.push.title + " : " + data.push.body.substr(0, 25) + '.....</li>');
+        var color = "#EEEEEE";
+        if (data.push.body == "Incoming call\n") {
+          color = "#4CAF50";
+        } else if (data.push.body == "On-going call\n") {
+          color = "#FFD54F";
+        } else if (data.push.package_name == "com.whatsapp") {
+          color = "#1B5E20";
+        } else if (data.push.package_name == "com.facebook.orca") {
+          color = "#0D47A1";
+        } else if (data.push.package_name == "com.bsb.hike") {
+          color = "#29B6F6";
+        }
+        $("#push_bullet ul").append('<li id ="' + data.push.notification_id + '" style="color: ' + color + ';">' + data.push.title + " : " + data.push.body.substr(0, 25) + '.....</li>');
         if ($('#push li').length > 2) {
           $("#push li")[0].remove();
         }
